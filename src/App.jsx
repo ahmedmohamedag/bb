f8c39e4d508e3e64e8c07f8c59fcf33739d2bcc3
Ahmed Mohamed <barmajli.info@gmail.com>
2024-06-23 00:55:23 +0300

    add2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header.jsx";
-import UserInput from "./components/Userinput.jsx";
+import UserInput from "./components/UserInput.jsx";
 import Results from "./components/Results.jsx";
 
 function App() {
